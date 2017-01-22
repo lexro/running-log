19966ab111f945300415d1c4d965baba3e88b4d8
Lex Lacson <lex.lacson.dev@gmail.com>
2017-01-21 21:32:08 -0800

    more complicated password validation

--- app/application/route.js
+++ app/application/route.js
@@ -5,6 +5,7 @@ export default Ember.Route.extend({
     Ember.$(document).foundation();
 
     window.Foundation.Abide.defaults.validators['multiValidateText'] = this._multiValidateText;
+    window.Foundation.Abide.defaults.validators['passwordValidate'] = this._passwordValidate;
   },
 
   // based on abide to get the validation error type
@@ -43,7 +44,58 @@ export default Ember.Route.extend({
     return valid;
   },
 
-  setupController: function( controller, model ) {
+  _passwordValidate($el) {
+    // error if password doesn't have these
+    const positiveValidations = {
+      sybmol: [/[\!\@\#\$\%\^\&\*]/g, 'Should have a symbol (!, @, #, $, %, ^, &, *)'],
+      number: [/\d/g, 'Should have a number'],
+      lowercase: [/[a-z]/g, 'Should have a lowercase letter'],
+      uppercase: [/[A-Z]/g, 'Should have an uppercase letter']
+    };
+
+    // error if password has these
+    const negativeValidations = {
+      illegalCharacter: [/[^A-z0-9\!\@\#\$\%\^\&\*]/g, 'Illegal Character']
+    };
+
+    const minPasswordLength = 8;
+    let issue;
+    const password = $el.val();
+    let validationMessages = [];
+
+    if (password.length) {
+      if (password.length < minPasswordLength) {
+        validationMessages.push('Should be at least 8 characters');
+      }
+
+    } else {
+      validationMessages.push('A Password is required');
+    }
+
+    for (issue in positiveValidations) {
+      let validation = positiveValidations[issue];
+      if (!password.match(validation[0])) {
+        validationMessages.push(validation[1]);
+      }
+    }
+
+    for (issue in negativeValidations) {
+      let validation = negativeValidations[issue];
+      if (password.match(validation[0])) {
+        validationMessages.push(validation[1]);
+      }
+    }
+
+    const valid = !validationMessages.length;
+
+    if (!valid) {
+      $el.validationMessage = validationMessages.join('\n');
+    }
+
+    return valid;
+  },
+
+  setupController( controller, model ) {
     this._super(controller, model);
     Ember.run.scheduleOnce( 'afterRender', this._initFoundation.bind(this));
   },
