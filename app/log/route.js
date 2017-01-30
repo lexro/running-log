557d8140b3a8966f0d94cbefdb3010ef86359173
Lex Lacson <lex.lacson.dev@gmail.com>
2017-01-30 09:00:22 -0800

    end runs and save them

--- app/log/route.js
+++ app/log/route.js
@@ -1,80 +1,92 @@
 import Ember from 'ember';
+const stub = [
+  {
+    distance: 9,
+    time: 5400000, // in milliseconds
+    pace: '9\' 10"',
+    date: 1484602919979,
+    id: 1
+  },
+  {
+    distance: 9,
+    time: 5400000, // in milliseconds
+    pace: '9\' 10"',
+    date: 1484602919979,
+    id: 2
+  },
+  {
+    distance: 9,
+    time: 5400000, // in milliseconds
+    pace: '9\' 10"',
+    date: 1484602919979,
+    id: 3
+  },
+  {
+    distance: 9,
+    time: 5400000, // in milliseconds
+    pace: '9\' 10"',
+    date: 1484602919979,
+    id: 4
+  },
+  {
+    distance: 9,
+    time: 5400000, // in milliseconds
+    pace: '9\' 10"',
+    date: 1484602919979,
+    id: 5
+  },
+  {
+    distance: 9,
+    time: 5400000, // in milliseconds
+    pace: '9\' 10"',
+    date: 1484602919979,
+    id: 6
+  },
+  {
+    distance: 9,
+    time: 5400000, // in milliseconds
+    pace: '9\' 10"',
+    date: 1484602919979,
+    id: 7
+  },
+  {
+    distance: 9,
+    time: 5400000, // in milliseconds
+    pace: '9\' 10"',
+    date: 1484602919979,
+    id: 8
+  },
+  {
+    distance: 9,
+    time: 5400000, // in milliseconds
+    pace: '9\' 10"',
+    date: 1484602919979,
+    id: 9
+  },
+  {
+    distance: 9,
+    time: 5400000, // in milliseconds
+    pace: '9\' 10"',
+    date: 1484602919979,
+    id: 10
+  }
+];
 
 export default Ember.Route.extend({
   model() {
+    let runs = localStorage.getItem('run-log-runs');
+    // add some mock data to make it look full
+    if (!runs) {
+      runs = stub;
+    } else {
+      console.log('runs:', runs);
+      runs = JSON.parse(runs);
+      console.log('runs:', runs);
+      runs = runs.concat(stub);
+    }
+
     return {
-      runs: [
-        {
-          distance: 9,
-          time: 5400000, // in milliseconds
-          pace: '9\' 10"',
-          date: 1484602919979,
-          id: 1
-        },
-        {
-          distance: 9,
-          time: 5400000, // in milliseconds
-          pace: '9\' 10"',
-          date: 1484602919979,
-          id: 2
-        },
-        {
-          distance: 9,
-          time: 5400000, // in milliseconds
-          pace: '9\' 10"',
-          date: 1484602919979,
-          id: 3
-        },
-        {
-          distance: 9,
-          time: 5400000, // in milliseconds
-          pace: '9\' 10"',
-          date: 1484602919979,
-          id: 4
-        },
-        {
-          distance: 9,
-          time: 5400000, // in milliseconds
-          pace: '9\' 10"',
-          date: 1484602919979,
-          id: 5
-        },
-        {
-          distance: 9,
-          time: 5400000, // in milliseconds
-          pace: '9\' 10"',
-          date: 1484602919979,
-          id: 6
-        },
-        {
-          distance: 9,
-          time: 5400000, // in milliseconds
-          pace: '9\' 10"',
-          date: 1484602919979,
-          id: 7
-        },
-        {
-          distance: 9,
-          time: 5400000, // in milliseconds
-          pace: '9\' 10"',
-          date: 1484602919979,
-          id: 8
-        },
-        {
-          distance: 9,
-          time: 5400000, // in milliseconds
-          pace: '9\' 10"',
-          date: 1484602919979,
-          id: 9
-        },
-        {
-          distance: 9,
-          time: 5400000, // in milliseconds
-          pace: '9\' 10"',
-          date: 1484602919979,
-          id: 10
-        }
-      ]
+      runs: runs
     };
   }
 });
--- app/new-run/route.js
+++ app/new-run/route.js
@@ -6,6 +6,12 @@ export default Ember.Route.extend({
     controller.setProperties({
       isRunning: null,
 
+      distance: 0.0,
+
+      currentTime: 0,
+
+      pace: 0,
+
       actions: {
         startRun() {
           this.set('isRunning', true);
@@ -18,7 +24,27 @@ export default Ember.Route.extend({
         },
 
         endRun() {
-          this.set('endRun', true);
+          this.set('isRunning', false);
+          this.set('isPaused', true);
+
+          let runs = localStorage.getItem('run-log-runs');
+
+          if (runs) {
+            runs = JSON.parse(runs);
+          } else {
+            runs = [];
+          }
+
+          runs.unshift({
+            distance: this.get('distance'),
+            time: this.get('currentTime'),
+            pace: this.get('pace'),
+            date: Date.now(),
+            id: runs.length + 1
+          });
+
+          localStorage.setItem('run-log-runs', JSON.stringify(runs));
+          this.transitionToRoute('log');
         }
       }
     });
